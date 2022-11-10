23cd1ae24d4f5207039a38284b6c3c2d941632ed
ofadiman <ofadiman@gmail.com>
2022-11-10 12:35:56 +0100

    feat: 175

---
+++ database/175.sql
@@ -0,0 +1,28 @@
+create table people
+(
+    person_id  int  not null,
+    last_name  text not null,
+    first_name text not null
+);
+
+insert into people (person_id, last_name, first_name)
+values (1, 'Wang', 'Allen'),
+       (2, 'Alice', 'Bob');
+
+create table addresses
+(
+    address_id int not null,
+    person_id  int not null,
+    city       text,
+    state      text
+);
+
+insert into addresses (address_id, person_id, city, state)
+values (1, 2, 'New York City', 'New York'),
+       (2, 3, 'Leetcode', 'California');
+
+-- Solution
+select first_name, last_name, city, state
+from people
+         left join addresses using (person_id);
+-- Solution
