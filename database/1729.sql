1cf25849a09171e801a679be9447d6aa2d1470fe
ofadiman <ofadiman@gmail.com>
2022-11-10 23:36:06 +0100

    feat: 1729

---
+++ database/1729.sql
@@ -0,0 +1,18 @@
+create table followers
+(
+    user_id     int,
+    follower_id int
+);
+
+insert into followers (user_id, follower_id)
+values (0, 1),
+       (1, 0),
+       (2, 0),
+       (2, 1);
+
+-- Solution
+select user_id, count(*) as followers_count
+from followers
+group by user_id
+order by user_id;
+-- Solution
