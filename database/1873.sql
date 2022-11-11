0f1d14d0438517beacf60354cd07a0662bad76d7
ofadiman <ofadiman@gmail.com>
2022-11-11 09:51:29 +0100

    feat: 1873

---
+++ database/1873.sql
@@ -0,0 +1,22 @@
+create table employees
+(
+    employee_id int,
+    name        varchar,
+    salary      int
+);
+
+insert into employees (employee_id, name, salary)
+values (2, 'Meir', 3000),
+       (3, 'Michael', 3800),
+       (7, 'Addilyn', 7400),
+       (8, 'Juan', 6100),
+       (9, 'Kannon', 7700);
+
+-- Solution 1
+select employee_id,
+       case
+           when employees.employee_id % 2 = 0 or employees.name like 'M%' then 0
+           else employees.salary end as bonus
+from employees
+order by employee_id;
+-- Solution 1
