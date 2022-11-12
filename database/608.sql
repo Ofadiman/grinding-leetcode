d2d618bf087396eb6c1efbbe29ea5b9dfd92a6dc
ofadiman <ofadiman@gmail.com>
2022-11-12 14:00:42 +0100

    feat: 608

---
+++ database/608.sql
@@ -0,0 +1,25 @@
+create table tree
+(
+    id        int,
+    parent_id int
+);
+
+insert into tree (id, parent_id)
+values (1, null),
+       (2, 1),
+       (3, 1),
+       (4, 2),
+       (5, 2);
+
+-- Solution 1
+select distinct tree1.id,
+                case
+                    when tree1.parent_id is null then 'Root'
+                    when tree1.parent_id is not null and tree2.id is null then 'Leaf'
+                    else 'Inner'
+                    end
+                    as type
+from tree as tree1
+         left join tree as tree2 on tree1.id = tree2.parent_id
+order by tree1.id;
+-- Solution 1
