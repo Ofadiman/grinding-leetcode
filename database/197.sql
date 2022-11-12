df6212c3ddaa99eabb8405f2703a3baee423dfe3
ofadiman <ofadiman@gmail.com>
2022-11-12 12:08:47 +0100

    feat: 197

---
+++ database/197.sql
@@ -0,0 +1,33 @@
+create table weather
+(
+    id          int,
+    record_date date,
+    temperature int
+);
+
+insert into weather (id, record_date, temperature)
+values (1, '2015-01-01', 10),
+       (2, '2015-01-02', 25),
+       (3, '2015-01-03', 20),
+       (4, '2015-01-04', 30),
+       (5, '2015-01-06', 35),
+       (6, '2015-01-08', 30);
+
+-- Solution 1
+with cte as (select weather.*,
+                    lag(temperature) over (order by weather.record_date) as previous_temperature,
+                    lag(record_date) over (order by weather.record_date) as previous_date
+             from weather)
+select *
+from cte
+where cte.record_date - cte.previous_date = 1
+  and cte.temperature > cte.previous_temperature;
+-- Solution 1
+
+-- Solution 2
+select *
+from weather weather1,
+     weather weather2
+where weather1.temperature > weather2.temperature
+  and weather1.record_date - weather2.record_date = 1;
+-- Solution 2
