20c85ee30f4f142a1d9f25acc0e0e8b4b02ac755
ofadiman <ofadiman@gmail.com>
2022-11-13 11:13:55 +0100

    feat: 178

---
+++ database/178.sql
@@ -0,0 +1,18 @@
+create table scores
+(
+    id    int,
+    score decimal
+);
+
+insert into scores (id, score)
+values (1, 3.50),
+       (2, 3.65),
+       (3, 4.00),
+       (4, 3.85),
+       (5, 4.00),
+       (6, 3.65);
+
+-- Solution 1
+select score, dense_rank() over (order by score desc) as rank
+from scores;
+-- Solution 1
