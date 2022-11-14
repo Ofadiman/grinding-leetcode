7aedb50e2c9bc5b11d259b032b809fd34144b0c9
ofadiman <ofadiman@gmail.com>
2022-11-14 22:12:26 +0100

    feat: 1365

---
+++ hash-table/1365.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func smallerNumbersThanCurrent1(nums []int) []int {
+	sortedNums := make([]int, len(nums))
+	copy(sortedNums, nums)
+	sort.Ints(sortedNums)
+	counts := make([]int, len(nums))
+	for i, num := range nums {
+		counts[i] = sort.Search(len(sortedNums), func(j int) bool {
+			return num <= sortedNums[j]
+		})
+	}
+	return counts
+}
+
+func main() {
+	fmt.Println(smallerNumbersThanCurrent1([]int{8, 1, 2, 2, 3}))
+}
