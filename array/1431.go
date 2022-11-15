c2e82f75051c5fe0060add3794d6ae635ea1a990
ofadiman <ofadiman@gmail.com>
2022-11-15 22:02:32 +0100

    feat: 1431

---
+++ array/1431.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func kidsWithCandies1(candies []int, extraCandies int) []bool {
+	highestCandyCount := 0
+	for i := 0; i < len(candies); i++ {
+		highestCandyCount = int(math.Max(float64(candies[i]), float64(highestCandyCount)))
+	}
+
+	result := make([]bool, len(candies))
+	for i := 0; i < len(candies); i++ {
+		if candies[i]+extraCandies >= highestCandyCount {
+			result[i] = true
+		} else {
+			result[i] = false
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(kidsWithCandies1([]int{2, 3, 5, 1, 3}, 3))
+}
