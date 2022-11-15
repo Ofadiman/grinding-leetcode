df103129b14a8877b4209fa66377b4ac3d9bba2a
ofadiman <ofadiman@gmail.com>
2022-11-15 21:36:05 +0100

    feat: 177

---
+++ database/177.sql
@@ -0,0 +1,32 @@
+create table employees
+(
+    id     int,
+    salary int
+);
+
+insert into employees (id, salary)
+values (1, 100),
+       (2, 200),
+       (3, 300);
+
+-- Solution 1
+create function get_nth_highest_value(table_name regclass, column_name text, n integer)
+    returns int
+    language plpgsql
+as
+$$
+declare
+    highest_value integer;
+begin
+    execute format(
+                        'with cte as (select *, dense_rank() over (order by %s desc) as dense_ranked from %s) ' ||
+                        'select %s ' ||
+                        'from cte ' ||
+                        'where cte.dense_ranked = %s;',
+                        column_name, table_name, column_name, n) into highest_value;
+    return highest_value;
+end;
+$$;
+
+select get_nth_highest_value('employees', 'salary', 2);
+-- Solution 1
