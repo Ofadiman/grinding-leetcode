de364745c03fec20a74e7cec16df87a04eb69402
ofadiman <ofadiman@gmail.com>
2022-11-18 12:25:33 +0100

    feat: 1656

---
+++ array/1656.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type OrderedStream struct {
+	length  int
+	chunks  []string
+	current int
+}
+
+func Constructor(n int) OrderedStream {
+	return OrderedStream{
+		length:  n,
+		chunks:  make([]string, n),
+		current: 0,
+	}
+}
+
+func (this *OrderedStream) Insert(idKey int, value string) []string {
+	index := idKey - 1
+
+	this.chunks[index] = value
+
+	if index > this.current {
+		return this.chunks[:0]
+	}
+
+	for i := this.current; i < this.length && this.chunks[i] != ""; i++ {
+		this.current++
+	}
+
+	return this.chunks[idKey-1 : this.current]
+}
+
+func main() {
+	stream := Constructor(5)
+	r1 := stream.Insert(3, "ccccc")
+	fmt.Println(r1)
+	r2 := stream.Insert(1, "aaaaa")
+	fmt.Println(r2)
+	r3 := stream.Insert(2, "bbbbb")
+	fmt.Println(r3)
+	r4 := stream.Insert(5, "eeeee")
+	fmt.Println(r4)
+	r5 := stream.Insert(4, "ddddd")
+	fmt.Println(r5)
+}
