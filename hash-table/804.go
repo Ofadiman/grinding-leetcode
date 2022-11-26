4430beb345f4f11eeb1ca6e30463a44b19be2f18
ofadiman <ofadiman@gmail.com>
2022-11-26 19:58:26 +0100

    feat: 804

---
+++ hash-table/804.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func uniqueMorseRepresentations(words []string) int {
+	morseCodes := map[string]string{
+		"a": ".-",
+		"b": "-...",
+		"c": "-.-.",
+		"d": "-..",
+		"e": ".",
+		"f": "..-.",
+		"g": "--.",
+		"h": "....",
+		"i": "..",
+		"j": ".---",
+		"k": "-.-",
+		"l": ".-..",
+		"m": "--",
+		"n": "-.",
+		"o": "---",
+		"p": ".--.",
+		"q": "--.-",
+		"r": ".-.",
+		"s": "...",
+		"t": "-",
+		"u": "..-",
+		"v": "...-",
+		"w": ".--",
+		"x": "-..-",
+		"y": "-.--",
+		"z": "--..",
+	}
+
+	table := map[string]int32{}
+	for _, word := range words {
+		var slice []string
+		for _, character := range word {
+			slice = append(slice, morseCodes[string(character)])
+		}
+
+		morseWord := strings.Join(slice, "")
+
+		if _, ok := table[morseWord]; ok {
+			table[morseWord]++
+		} else {
+			table[morseWord] = 0
+		}
+	}
+
+	return len(table)
+}
+
+func main() {
+	fmt.Println(uniqueMorseRepresentations([]string{"gin", "zen", "gig", "msg"}))
+}
