ceb5ecea19c8158ad1413cb7a1f3a66127390c49
ofadiman <ofadiman@gmail.com>
2022-11-26 22:43:19 +0100

    feat: 1475

---
+++ hash-table/1475.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func finalPrices(prices []int) []int {
+	slice := make([]int, len(prices))
+	var stack []int
+
+	for i := len(prices) - 1; i >= 0; i-- {
+		for len(stack) > 0 && prices[i] < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		}
+
+		if len(stack) == 0 {
+			stack = append(stack, prices[i])
+			slice[i] = prices[i]
+		} else {
+			slice[i] = prices[i] - stack[len(stack)-1]
+			stack = append(stack, prices[i])
+		}
+	}
+	return slice
+}
+
+func main() {
+	fmt.Println(finalPrices([]int{8, 4, 6, 2, 3}))
+}
