2c110ce9c6020b8a206185278d81148a35902add
ofadiman <ofadiman@gmail.com>
2022-11-27 19:54:03 +0100

    feat: 1720

---
+++ array/1720.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func decode(encoded []int, first int) []int {
+	result := make([]int, len(encoded)+1)
+	result[0] = first
+
+	for i, number := range encoded {
+		first ^= number
+		result[i+1] = first
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(decode([]int{1, 2, 3}, 1))
+}
