7ad57569def2e317364615151d7c37d45cbb5ae9
ofadiman <ofadiman@gmail.com>
2022-12-05 18:40:40 +0100

    feat: 1874

---
+++ array/1874.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func minProductSum(nums1 []int, nums2 []int) int {
+	table := make(map[int][]int)
+	for i := 1; i <= 100; i++ {
+		table[i] = []int{}
+	}
+
+	for i, number := range nums2 {
+		table[number] = append(table[number], i)
+	}
+
+	var order []int
+	for i := 1; i <= 100; i++ {
+		order = append(order, table[i]...)
+	}
+
+	sort.Ints(nums1)
+
+	sum := 0
+	for i := 0; i < len(nums1); i++ {
+		sum += nums1[i] * nums2[order[len(nums1)-1-i]]
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(minProductSum([]int{5, 3, 4, 2}, []int{4, 2, 2, 5}))
+}
